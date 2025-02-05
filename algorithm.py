1b7463e35cb5d0194879074e2ddcc6f0696c4b7d
Maximilian Wolfe <maximilianwolfe117@gmail.com>
2025-02-05 15:14:19 -0800

    fixed base cases

--- algorithm.py
+++ algorithm.py
@@ -5,14 +5,6 @@ def edit_distance_with_backtrace(s, t):
     m, n = len(s), len(t)
     D = np.zeros((m+1, n+1), dtype=int)
     ptr = np.zeros((m+1, n+1), dtype=tuple)
-    
-    # Initialize base cases
-    for i in range(1, m+1):
-        D[i][0] = i
-        ptr[i][0] = (i-1, 0)
-    for j in range(1, n+1):
-        D[0][j] = j
-        ptr[0][j] = (0, j-1)
 
     #TO DO: read in cost table as referance
     # open the sample file used 
@@ -21,6 +13,17 @@ def edit_distance_with_backtrace(s, t):
     # read the content of the file opened 
     content = file.readlines() 
 
+    # Initialize base cases
+    for i in range(1, m+1):
+        columnIndex = content[0].index(s[i-1])
+        tablecost = int(content[1][columnIndex])
+        D[i][0] = tablecost + D[i-1][0]
+        ptr[i][0] = (i-1, 0)
+    for j in range(1, n+1):
+        columnIndex = content[0].index(t[j-1])
+        tablecost = int(content[1][columnIndex])
+        D[0][j] = tablecost + D[0][j-1]
+        ptr[0][j] = (0, j-1)
     
     
     # Fill DP table
@@ -34,7 +37,6 @@ def edit_distance_with_backtrace(s, t):
                        (D[i-1][j-1] + tablecost, (i-1, j-1))]  # diagonal
             
             D[i][j], ptr[i][j] = min(choices, key=lambda x: x[0])
-
     #TO DO: write the table as a file
     for i in range(0, m+1):
         lin=""
@@ -62,7 +64,14 @@ def edit_distance_with_backtrace(s, t):
     return D[m][n], "".join(aligned_s[::-1]), "".join(aligned_t[::-1])
 
 # testing
-s1, s2 = "CGCAATTCTGAAGCGCTGGGGAAGACGGGT", "TATCCCATCGAACGCCTATTCTAGGAT"
+s1, s2 = "ATGC", "ATGC"
 distance, aligned_s1, aligned_s2 = edit_distance_with_backtrace(s1.replace('-', ''), s2.replace('-', ''))
 print(f"Edit Distance: {distance}")
 print(f"Alignment:\n{aligned_s1}\n{aligned_s2}")
+
+"""*,-,A,T,G,C
+    -,0,1,2,1,3
+    A,1,0,1,5,1
+    T,2,1,0,9,1
+    G,1,5,9,0,1
+    C,3,1,1,1,0"""
